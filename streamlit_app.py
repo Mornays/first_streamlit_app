50d102298070149e159eef9489aff094e6d1eae3
Mornays <mornaysymington@gmail.com>
2023-08-11 11:19:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,10 +57,3 @@ if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list
     streamlit.dataframe(my_data_rows)
-
-# Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
-
-# This will not work for now
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
